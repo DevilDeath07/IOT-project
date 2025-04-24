2a329415ad76946c51716b952798a3c7c467970d
Balamurugan V <balamurugankumar880@gmail.com>
2025-04-24 22:29:45 +0530

    arduino, C++, capacitor, seven segment display, LED's, breadboards and others
    Traffic light with timer and that timer is seven segment display is used....

---
+++ projects/trafficlight_with_timer.ino
@@ -0,0 +1,258 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT); //red
+  pinMode(12, OUTPUT); //yellow
+  pinMode(11, OUTPUT); //green
+
+  pinMode(2,OUTPUT); //a
+  pinMode(3,OUTPUT); //b
+  pinMode(4,OUTPUT); //c
+  pinMode(5,OUTPUT); //d
+  pinMode(6,OUTPUT); //e
+  pinMode(7,OUTPUT); //f
+  pinMode(8,OUTPUT); //g
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+      digitalWrite(13, HIGH);
+  
+  
+
+
+   //9
+   digitalWrite(2,LOW); 
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,LOW);
+   digitalWrite(8, LOW);
+
+   delay(1000);
+   //8
+      digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
+   digitalWrite(7,LOW);
+   digitalWrite(8, LOW);
+
+   delay(1000);
+   //7
+      digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,HIGH);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,HIGH);
+   digitalWrite(8, HIGH);
+
+   delay(1000);
+  //6
+     digitalWrite(2,LOW);
+   digitalWrite(3,HIGH);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
+   digitalWrite(7,LOW);
+   digitalWrite(8, LOW);
+
+   delay(1000);
+  //5
+     digitalWrite(2,LOW);
+   digitalWrite(3,HIGH);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,LOW);
+   digitalWrite(8, LOW);
+   
+   digitalWrite(12, HIGH);
+    delay(400);
+    digitalWrite(12, LOW);
+    delay(1000);
+
+
+  //4
+     digitalWrite(2,HIGH);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,HIGH);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,LOW);
+   digitalWrite(8, LOW);
+   digitalWrite(12, HIGH);
+     delay(500);
+   digitalWrite(12, LOW);
+       delay(1000);
+
+  //3
+     digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,HIGH);
+   digitalWrite(8, LOW);
+   digitalWrite(12, HIGH);
+     delay(600);
+   digitalWrite(12, LOW);
+      delay(1000);
+
+
+  //2
+     digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,HIGH);
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
+   digitalWrite(7,HIGH);
+   digitalWrite(8, LOW);
+   digitalWrite(12, HIGH);
+       delay(700);
+   digitalWrite(12, LOW);
+      delay(1000);
+   
+
+  //1
+     digitalWrite(2,HIGH);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,HIGH);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,HIGH);
+   digitalWrite(8,HIGH);
+   digitalWrite(12, HIGH);
+    delay(800);
+   digitalWrite(12, LOW);
+      delay(1000);
+
+
+  //0
+     digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
+   digitalWrite(7,LOW);
+   digitalWrite(8, HIGH);
+      digitalWrite(13,LOW);
+  
+   digitalWrite(12, HIGH);
+    delay(2000);
+   digitalWrite(12, LOW);
+
+
+
+
+
+
+
+    digitalWrite(11, HIGH);
+
+       //9
+   digitalWrite(2,LOW); 
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,LOW);
+   digitalWrite(8, LOW);
+
+   delay(1000);
+   //8
+      digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
+   digitalWrite(7,LOW);
+   digitalWrite(8, LOW);
+
+   delay(1000);
+   //7
+      digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,HIGH);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,HIGH);
+   digitalWrite(8, HIGH);
+
+   delay(1000);
+  //6
+     digitalWrite(2,LOW);
+   digitalWrite(3,HIGH);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
+   digitalWrite(7,LOW);
+   digitalWrite(8, LOW);
+
+   delay(1000);
+  //5
+     digitalWrite(2,LOW);
+   digitalWrite(3,HIGH);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,LOW);
+   digitalWrite(8, LOW);
+
+   delay(1000);
+  //4
+     digitalWrite(2,HIGH);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,HIGH);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,LOW);
+   digitalWrite(8, LOW);
+
+   delay(1000);
+  //3
+     digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,HIGH);
+   digitalWrite(8, LOW);
+
+   delay(1000);
+  //2
+     digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,HIGH);
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
+   digitalWrite(7,HIGH);
+   digitalWrite(8, LOW);
+
+   delay(1000);
+  //1
+     digitalWrite(2,HIGH);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,HIGH);
+   digitalWrite(6,HIGH);
+   digitalWrite(7,HIGH);
+   digitalWrite(8,HIGH);
+
+   delay(1000);
+  //0
+     digitalWrite(2,LOW);
+   digitalWrite(3,LOW);
+   digitalWrite(4,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
+   digitalWrite(7,LOW);
+   digitalWrite(8, HIGH);
+
+   delay(1000);
+ 
+  digitalWrite(11,LOW);
+}
